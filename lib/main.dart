e0406398dce8f2042565957f1bcee1997c984986
sem benavente <sembenavente@gmail.com>
2020-12-21 02:09:40 -0500

    changing tittle

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Hello world'),
+      home: MyHomePage(title: 'El mapita de Seen'),
     );
   }
 }
